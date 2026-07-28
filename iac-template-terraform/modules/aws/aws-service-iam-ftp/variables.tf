dc6f43004e355556085dfb2d9132625a95a4588b
Brayan Hernandez <brayan.hernandez@ux.technology>
2026-07-28 12:02:34 -0500

    feat: deploy ftp

--- iac-template-terraform/modules/aws/aws-service-iam-ftp/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "iam_role_name" {
-  description = "IAM Role Name"
-  type        = string
-}
-
-variable "s3_bucket_arn" {
-  description = "ARN del bucket S3 FTP"
-  type        = string
-}
